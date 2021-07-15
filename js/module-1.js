9008de227cadeed5df1212692036b30839b0b71f
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-07-15 22:14:15 +0300

    primitives

---
+++ js/module-1.js
@@ -0,0 +1,45 @@
+console.log(1);
+
+let stringData = "string data";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = "";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = " ";
+console.log(stringData)
+console.log(typeof stringData);
+
+let number = 1;
+console.log(number);
+console.log(typeof number);
+
+number = 2e89;
+console.log(number);
+console.log(typeof number);
+
+number = Infinity;
+console.log(number);
+console.log(typeof number);
+
+number = NaN;
+console.log(number);
+console.log(typeof number);
+
+let isOnline = true;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+let message;
+console.log(message);
+console.log(typeof message);
+
+let user = null;
+console.log(user);
+console.log(typeof user);
